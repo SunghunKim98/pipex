a5cc60802d100f457f8e7d8906851306b039ab49
Sunghun Kim <sungkim@c10r7s13.42seoul.kr>
2021-07-09 17:44:28 +0900

    ls

--- main.c
+++ main.c
@@ -55,6 +55,12 @@ static void cmd_init(const char *cmd, t_cmd *p)
 		printf("%d: %s\n", i, arr[i]);
 }
 
+void	fork_fail()
+{
+	perror("Fork fail");
+	exit(1);
+}
+
 static void	execute_command(t_cmd cmd)
 {
 	int		i;
@@ -63,6 +69,40 @@ static void	execute_command(t_cmd cmd)
 	while (++i < 5)
 		execve(cmd.cmd[i], cmd.argv, cmd.envp);
 	perror(cmd.argv[0]);
+}
+
+static void child_re(int *pipe_stuff, int index)
+{
+	pid_t   pid;
+	int     pipe_re[2];
+	t_cmd	cmd;
+	int		status;
+
+	if (index == 2)
+	{
+		dup2(pipe_stuff[STDOUT_FILENO], STDOUT_FILENO) // pipe로 넘기고
+		// execute(); -> pipe_re[]
+	}
+	else
+	{
+		pipe(pipe_re);
+		pid = fork();
+		if (pid > 0) // parent
+		{
+			waitpid(pid, &status, 0); // 자식 프로세스가 종료될 때까지 여기서 Block상태로 대기.
+			if (WIFSIGNALED(status)) // 비정상 종료
+				exit(1);
+			dup2(pipe_re[STDIN_FILENO], STDIN_FILENO)	// pipe로 받아와서
+			// execute
+		}
+		else if (pid == 0) // child
+		{
+			child_re(pipe_re); // 재귀타러 들어감.
+		}
+		else
+			fork_fail();
+	}
+
 }
 
 int main(int argc, char **argv)
@@ -74,26 +114,56 @@ int main(int argc, char **argv)
 
 	pipe(pipefd);
 	pid = fork();
+
 	if (pid > 0) // parent
 	{
-		waitpid(pid, &status, 0); // 자식 프로세스가 종료될 때까지 여기서 Block상태로 대기.
-		if (WIFSIGNALED(status)) // 비정상 종료
-			exit(1);
 
-		redirect_out(argv[4]); // 여기서 출력되는 것들은 파일에 쓰겠다는 것을 의미 -> stdout의 화살표 방향을 바꿈.
-		dup2(pipefd[0], 0); // 자식 프로세스에서 보낸 것을 pipefd[0]을 통해 받아온다.
-		close(pipefd[1]);
-		cmd_init(argv[3], &cmd);
-		execute_command(cmd); // 명령어를 실행하면, pipedfd[0]에서 read한 것들을 argv[4]의 파일에 쓰게 된다.
 	}
 	else if (pid == 0) // child
 	{
-		redirect_in(argv[1]); // 여기서 어떤 파일을 읽어옴.
-		close(pipefd[0]);
-		dup2(pipefd[STDOUT_FILENO], STDOUT_FILENO); // 그 여기서 출력되는 부분들은 pipefd[1]로 들어간다.
-		cmd_init(argv[2], &cmd);
-		execute_command(cmd); // 여기서 실행하면 어떤 출력의 결과가 생기고 그 출력의 결과들은 pipefd[1]을 타고 들어간다.
+
+		child_re(pipefd[1]);
 	}
+	else
+		fork_fail();
+
+
+
+	// pipe(pipefd);
+	// pid = fork();
+
+	// if (pid > 0) // parent
+	// {
+	// 	waitpid(pid, &status, 0); // 자식 프로세스가 종료될 때까지 여기서 Block상태로 대기.
+	// 	if (WIFSIGNALED(status)) // 비정상 종료
+	// 		exit(1);
+	// 	perror("asd");
+	// 	redirect_out(argv[4]); // 여기서 출력되는 것들은 파일에 쓰겠다는 것을 의미 -> stdout의 화살표 방향을 바꿈.
+	// 	dup2(pipefd[STDIN_FILENO], STDIN_FILENO); // 자식 프로세스에서 보낸 것을 pipefd[0]을 통해 받아온다.
+	// 	close(pipefd[1]);
+	// 	cmd_init(argv[3], &cmd);
+	// 	execute_command(cmd); // 명령어를 실행하면, pipedfd[0]에서 read한 것들을 argv[4]의 파일에 쓰게 된다.
+	// }
+	// else if (pid == 0) // child
+	// {
+	// 	perror("asd");
+	// 	redirect_in(argv[1]); // 여기서 어떤 파일을 읽어옴.
+	// 	close(pipefd[0]);
+	// 	dup2(pipefd[STDOUT_FILENO], STDOUT_FILENO); // 그 여기서 출력되는 부분들은 pipefd[1]로 들어간다.
+	// 	cmd_init(argv[2], &cmd);
+	// 	execute_command(cmd); // 여기서 실행하면 어떤 출력의 결과가 생기고 그 출력의 결과들은 pipefd[1]을 타고 들어간다.
+	// }
+	// else
+	// {
+	// 	perror("Fork fail");
+	// 	exit(1);
+	// }
+
+
+
+
+
+
 
 
 	// pid_t	pid;
